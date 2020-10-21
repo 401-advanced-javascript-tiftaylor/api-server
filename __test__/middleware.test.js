133e799c44924fe9bdf8034b75ef5229b3ba19c7
Tif Taylor <tiftaylorisrad@gmail.com>
2020-10-20 17:19:26 -0700

    Added | middleware, server, updated index, eslintfiles, etc

---
+++ __test__/middleware.test.js
@@ -0,0 +1,17 @@
+const timeStamp = require('../middleware/timestamp.js')
+const logger = require('../middleware/logger.js')
+const notFound = require('../middleware/404.js')
+const serverErr = require('../middleware/500.js');
+const { TestScheduler } = require('jest');
+
+require('supertest');
+
+describe('????', () => {
+
+  test('???', () => {
+
+  })
+
+
+
+})
--- index.js
+++ index.js
@@ -1,24 +1,8 @@
 
 'use strict';
 
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('data/db.json');
-const middlewares = jsonServer.defaults();
+require('dotenv').config()
 
-const PORT = process.env.PORT || 3000;
-
-
-router.render = (req, res) => {
-  res.jsonp({
-    results: res.locals.data,
-    count: res.locals.data.length,
-  })
-}
-
-
-server.use(middlewares);
-server.use(router);
-server.listen(PORT, () => {
-  console.log('Server is running');
-});
+const server = require('./lib/server.js');
+const PORT = process.env.PORT || 3003;
+server.start(PORT);
